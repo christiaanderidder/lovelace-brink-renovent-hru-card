befbd7e70145422c8dd9d658ed1a5352ba76b6de
Christiaan de Ridder <christiaan@channelengine.com>
2024-07-10 13:22:27 +0200

    Add house outline and fan state buttons

--- card.js
+++ card.js
@@ -11,34 +11,87 @@ class RenoventHruCard extends LitElement {
     static get properties() {
         return {
             _config: { state: true },
-            _state: { state: true },
+            _fanMode: { state: true },
         };
     }
 
+    static styles = css`
+        .house {
+            width: 100%;
+            max-width: 400px;
+            margin: 0;
+        }
+
+        .house .roof {
+            z-index: 0;
+            height: 100%;
+            width: 100%;
+            height: 40px;
+        }
+
+        .house .roof svg {
+            width: 100%;
+            height: 100%;
+        }
+
+        .house .roof svg #stroke {
+            fill: white;
+            stroke: red;
+            stroke-width: 3px;
+        }
+
+        .house .roof svg #triangle1 {
+            fill: var(--primary-text-color);
+        }
+
+        .house .roof svg #triangle2 {
+            fill: var(--card-background-color);
+        }
+
+        .house .structure {
+            border: 3px solid var(--primary-text-color);    
+            background: var(--card-background-color);            
+            border-top: 0 none;
+            padding: 5px;
+        }
+                
+    `;
+
     setConfig(config) {
-        this._entity = config.entity;
+        this._fanModeEntity = config.fanModeEntity;
     }
 
     set hass(hass) {
         this._hass = hass;
-        this._state = hass.states[this._entity];
+        this._fanMode = hass.states[this._fanModeEntity];
     }
 
-    render () {
+    render() {
+        console.log(this._fanMode);
         let content;
-        if (!this._state) {
+        if (!this._fanMode) {
             content = html`
                 <p class="error">
-                    ${this._entity} is unavailable.
+                    ${this._fanModeEntity} is unavailable.
                 </p>
             `;
         } else {
             content = html`
-                <p>The ${this._entity} is ${this._state.state}.</p>
+                <div class="house">
+                    <div class="roof">
+                        <svg preserveAspectRatio="none" viewBox="0 0 400 45">
+                            <polygon id="triangle1" points="400,45 400,40 200,0 0,40 0,45"></polygon>
+                            <polygon id="triangle2" points="397,45 397,42 200,3 3,42 3,45"></polygon>
+                        </svg>
+                    </div>
+                    <div class="structure">
+                        ${this.renderFanModes()}
+                    </div>
+                </div>
             `;
         }
         return html`
-            <ha-card header="Hello ${this._hass.user.name}!">
+            <ha-card header="Renovent HRU">
                 <div class="card-content">
                     ${content}
                 </div>
@@ -46,8 +99,34 @@ class RenoventHruCard extends LitElement {
         `;
     }
 
+    fanModes = [
+        {label: 'Auto', value: 'Auto', icon: 'mdi:fan-auto'},
+        {label: 'Holiday', value: 'Holiday', icon: 'mdi:fan-off'},
+        {label: 'Reduced', value: 'Reduced', icon: 'mdi:fan-speed-1'},
+        {label: 'Normal', value: 'Normal', icon: 'mdi:fan-speed-2'},
+        {label: 'High', value: 'High', icon: 'mdi:fan-speed-3'}
+    ];
+
+    setFanMode(e) {
+        console.log("setFanMode", e.currentTarget.value);
+    }
+
+    renderFanModes() {
+        return this.fanModes.map((button) =>
+            html`
+                <mwc-button
+                    .value=${button.value}
+                    ?active=${this._fanMode.state === button.value}
+                    @click=${this._changeFanSpeed}>
+                    <ha-icon
+                        .icon=${button.icon}>
+                    </ha-icon>
+                </mwc-button>
+            `);
+    }
+
     static getStubConfig() {
-        return { entity: "sun.sun" }
+        return { fanModeEntity: "sensor.ebusd_excellent400_fanmode" }
     }
 }
 
