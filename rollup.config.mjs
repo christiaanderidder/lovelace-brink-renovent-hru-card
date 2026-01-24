ddea762d0123cc1629a8c5eb60e4fc5e137aa6cf
Christiaan de Ridder <christiaan@channelengine.com>
2026-01-24 14:08:09 +0100

    Remove rollup filesize plugin which has dependencies with vulnerabilities and is unmaintained

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,6 @@
 import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import filesize from "rollup-plugin-filesize";
 import typescript from "@rollup/plugin-typescript";
 import serve from "rollup-plugin-serve";
 import styles from "rollup-plugin-styler";
@@ -37,7 +36,6 @@ export default {
     }),
     json(),
     nodeResolve(),
-    filesize(),
     typescript(),
     ...(production ? [terser()] : [serve(serveOptions)]),
   ],
